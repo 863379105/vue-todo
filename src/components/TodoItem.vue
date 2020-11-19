7b29c8a9b7d9747f5b8f07e8dd2151adfe33222f
lb <863379105@qq.com>
2020-11-19 16:32:53 +0800

    rebuild

---
+++ src/components/TodoItem.vue
@@ -0,0 +1,36 @@
+<template>
+    <li :class="{'completed':todo.status}">
+        {{todo.content}}
+        <button class="delete-btn" @click="deleteTodo(todo.id)">删除</button>
+        <button class="complete-btn" @click="completeTodo(todo.id)">完成</button>
+    </li>
+</template>
+<script>
+    export default {
+        name : 'TodeItem',
+        props : ['todo'],
+        methods : {
+            deleteTodo(id){
+                this.$emit('deleteTodo',id)
+            },
+            completeTodo(id){
+            
+               this.$emit('completeTodo',id)
+            },
+            
+        }
+    }
+</script>
+<style  scoped>
+    .completed{
+        text-decoration: line-through;
+    }
+    .delete-btn{
+        position: absolute;
+        right: 0;
+    }
+    .complete-btn{
+        position: absolute;
+        right: 50px 
+    }
+</style>
--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -2,18 +2,24 @@
     <div id="todo-list">
         <input type="text" name="" id="" @keydown.enter="addList" v-model="inputContent">
         <ul>
-            <li v-for="item in todoList" :key="item.id" :class="{'completed':item.status}">
-                {{item.content}}
-                <button class="delete-btn" @click="deleteTodo(item.id)">删除</button>
-                <button class="complete-btn" @click="completeTodo(item.id)">完成</button>
-            </li>
+            <todo-item 
+                v-for="todo in todoList" 
+                :key="todo.id" 
+                :todo = 'todo' 
+                @deleteTodo='deleteTodo' 
+                @completeTodo='completeTodo'>
+            </todo-item>
         </ul>
     </div>
 </template>
 
 <script>
+import TodoItem from './TodoItem'
 export default {
     name : 'TodoList',
+    components:{
+        TodoItem
+    },
     data() {
         return {
             inputContent : '',
@@ -35,7 +41,6 @@ export default {
                 content : this.inputContent,
                 status : false
             }
-            console.log(newTodo);
             this.todoList.push(newTodo)
             this.inputContent = ''
         },
@@ -50,13 +55,13 @@ export default {
             return (year + month + day + hour + minute + second + num)
         },
         deleteTodo(id){
+
             this.todoList = this.todoList.filter((item) => {
                 return item.id !== id
             })
         },
         completeTodo(id){
             this.todoList.forEach((item) => {
-                console.log('ok');
                 if(item.id === id){
                     item.status = !item.status
                 }
@@ -77,18 +82,9 @@ export default {
         border: 1px tomato solid;
         padding: 5px;
     }
-    .completed{
-        text-decoration: line-through;
-    }
+    
     ul li{
         position: relative;
     }
-    .delete-btn{
-        position: absolute;
-        right: 0;
-    }
-    .complete-btn{
-        position: absolute;
-        right: 50px 
-    }
+    
 </style>
